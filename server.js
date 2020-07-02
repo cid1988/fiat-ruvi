2e4a61e295beb903af175c2d758c6d6b2dc9aafc
Pablo <perellopablo1988@gmail.com>
2020-07-02 17:05:00 -0300

    Correcciones del server

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-app.use(express.static(__dirname + 'dist/registro-ventas'));
+app.use(express.static(__dirname + '/dist/registro-ventas'));
 app.get('*', function(req,res){
     res.sendFile(path.join(__dirname + '/dist/registro-ventas/index.html'));
     app.listen(process.env.PORT || 8080);
